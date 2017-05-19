3dc48b382ceb67a4f189939d18cb45e4759f7b53
Felipe J. L. Rita <felipelopesrita@hotmail.com>
2017-05-18 21:04:32 -0300

    Refactor max, min

--- js/model.js
+++ js/model.js
@@ -1,6 +1,5 @@
 function format (data) {
-  var vec = { vertices: [], points: [] };
-  data.split('\n').reduce(readLine, vec);
+  var vec = data.split('\n').reduce(readLine, { vertices: [], points: [] });
   var vMax = vec4(vec.max.x, vec.max.y, vec.max.z, 1)
   var vMin = vec4(vec.min.x, vec.min.y, vec.min.z, 1)
 
@@ -12,24 +11,15 @@ function format (data) {
     vec.vertices [i][3] = 1;
   })
 
-  return vec.points.concatAll(item => {
-    var n = Number(item)
-    return vec.vertices[ n-1 ]
-  })
+  return vec.points.concatAll(item => vec.vertices[ item-1 ])
 }
 
 function readLine (obj, line) {
   arr = line.split(' ').map(e => e.split('/')[0]).filter(el => el != "")
   var X = Number(arr[1]), Y = Number(arr[2]), Z = Number(arr[3]);
   if (arr[0] == 'v') {
-    obj.max = obj.max == undefined ? { x: X, y: Y, z: Z } : obj.max;
-    obj.min = obj.min == undefined ? { x: X, y: Y, z: Z } : obj.min;
-    obj.max.x = Math.max( obj.max.x, X )
-    obj.max.y = Math.max( obj.max.y, Y )
-    obj.max.z = Math.max( obj.max.z, Z )
-    obj.min.x = Math.min( obj.min.x, X )
-    obj.min.y = Math.min( obj.min.y, Y )
-    obj.min.z = Math.min( obj.min.z, Z )
+    reduce (obj, 'max', { x: X, y: Y, z: Z }, Math.max)
+    reduce (obj, 'min', { x: X, y: Y, z: Z }, Math.min)
     obj.vertices.push(vec4(X, Y, Z, 1))
   }
   else if (arr[0] == 'f')
@@ -37,8 +27,22 @@ function readLine (obj, line) {
   return obj;
 }
 
+function reduce (obj, name, point, fn) {
+  if (obj[name] == undefined)
+    return obj[name] = point
+  Object.keys(point).forEach((index, coord) => {
+    obj[name][index] = fn(obj[name][index], point[index])
+  })
+}
+
+function colorize () {
+  var size = 0.2 / points.length, color = 0
+  return points.map(item => [ 0.83 - color, 0.82 - color, 0.65 - (color += size) - size, 1 ])
+}
+
 axios.get(app.server + '/models/deer.obj')
   .then(function (response) {
+    console.log('OK')
     var v = format(response.data)
     points = v
     colors = colorize()
@@ -47,8 +51,3 @@ axios.get(app.server + '/models/deer.obj')
   .catch(function (error) {
     console.log(error);
   })
-
-function colorize () {
-  var size = 0.2 / points.length, color = 0
-  return points.map(item => [ 0.83 - color, 0.82 - color, 0.65 - (color += size) - size, 1 ])
-}
